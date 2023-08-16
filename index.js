ca8aa6fd7bc97c998437672cce4649a802c0bfec
Kryselle Matienzo <krysellematienzo@gmail.com>
2023-08-16 17:02:47 -0400

    refactor code

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ document.querySelector(".check").addEventListener("click", function () {
   // When guess is not a valid number
   if (input_number > 20 || input_number < 1 || !input_number) {
     messageDisplay("⛔️ Please select numbers between 1 and 20 only!");
-    // When guess is wrong
+    // When guess is wrong (too high or low)
   } else if (input_number !== random_number) {
     if (score > 1) {
       score--;
@@ -20,21 +20,20 @@ document.querySelector(".check").addEventListener("click", function () {
     } else {
       messageDisplay("💥 You lost the game!");
       document.querySelector(".score").innerText = 0;
-      document.querySelector(".highscore").innerText = 0;
       document.querySelector(".check").disabled = true;
     }
     //When guess is correct
   } else {
+    // When current score is more than the current high score, we then get the newest highscore with this message
     if (score > highscore) {
       highscore = score;
       messageDisplay(`🎉 Your high score is ${highscore}!`);
+      // When you guess the number right but your score is lower than the high score, you get these message
     } else {
       messageDisplay("🎉 You guessed it right!");
     }
-    document.querySelector("body").style.backgroundColor = "#AEE938";
+    resetMessage("#AEE938", random_number, true, true, input_number);
     document.querySelector(".highscore").innerText = highscore;
-    document.querySelector(".outputNumberText").innerHTML = random_number;
-    document.querySelector(".check").disabled = true;
   }
   console.log("Score: " + score);
 });
@@ -49,13 +48,18 @@ let reset_btn = document
     score = 20;
     random_number = Math.trunc(Math.random() * 20) + 1;
     messageDisplay("Start guessing...");
-    document.querySelector(".check").disabled = false;
+    resetMessage("#fff0e5", "?", false, false, "");
     document.querySelector(".score").innerText = score;
-    document.querySelector(".outputNumberText").innerHTML = "?";
-    document.querySelector("body").style.backgroundColor = "#fff0e5";
     console.log("New random number: " + random_number);
   });
 
+function resetMessage(backgroundCol, outputNum, checkBtn, inputStat, inputVal) {
+  document.querySelector("body").style.backgroundColor = backgroundCol;
+  document.querySelector(".outputNumberText").innerHTML = outputNum;
+  document.querySelector(".check").disabled = checkBtn;
+  document.querySelector(".inputNumberText").disabled = inputStat;
+  document.querySelector(".inputNumberText").value = inputVal;
+}
 /* ADDED SOME IMPROVEMENT
 1. Check button disabled once you lost the game - done
 */
